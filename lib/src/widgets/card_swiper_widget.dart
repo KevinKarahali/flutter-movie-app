2afe011a075c7556830157e3db407e86c5b4d8e9
Kevin Karahali <kevin.karahali@hotmail.com>
2022-07-06 13:06:22 +0200

    show posters

--- lib/src/widgets/card_swiper_widget.dart
+++ lib/src/widgets/card_swiper_widget.dart
@@ -9,25 +9,24 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final _screenSize = MediaQuery.of(context).size;
 
-
     return Container(
-      padding: EdgeInsets.only( top: 10.0),
+      padding: EdgeInsets.only(top: 10.0),
       child: Swiper(
         layout: SwiperLayout.STACK,
         itemWidth: _screenSize.width * 0.7,
         itemHeight: _screenSize.height * 0.5,
         itemBuilder: (BuildContext context, int index) {
-          return ClipRect(
-            child:FadeInImage(
-              image: NetworkImage(movies[index].getPosterImg()),
-              placeholder: AssetImage('assets/img/no-image.jpg'),
-            )
-          );
+          return ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: FadeInImage(
+                image: NetworkImage(movies[index].getPosterImg()),
+                placeholder: AssetImage('assets/img/no-image.jpg'),
+                fit: BoxFit.cover,
+              ));
         },
-        itemCount: 3,
+        itemCount: movies.length,
         // pagination: SwiperPagination(),
         // control: SwiperControl(),
       ),
